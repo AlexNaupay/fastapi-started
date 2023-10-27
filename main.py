7f934a0218248b59cd2f39d6e5a84420d16b4b0c
Alex Naupay <alexnaupay@gmail.com>
2023-10-26 22:14:03 -0500

    run with pm2

--- main.py
+++ main.py
@@ -10,3 +10,5 @@ def index():
 #  uvicorn main:app --reload  --port 5000 --host 0.0.0.0
 #  pip freeze > requirements.txt
 #  pip install -r requirements.txt
+
+#  pm2 start "uvicorn main:app --port 5000 --host 0.0.0.0" --name APP_NAME
