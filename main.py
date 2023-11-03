189e034c1f0e61b75222e3d55b5b160009b574af
Alex Naupay <alexnaupay@gmail.com>
2023-11-03 11:38:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,4 +11,8 @@ def index():
 #  pip freeze > requirements.txt
 #  pip install -r requirements.txt
 
+# https://www.uvicorn.org/deployment/
 #  pm2 start "uvicorn main:app --port 5000 --host 0.0.0.0" --name APP_NAME
+
+# Behind a proxy
+# pm2 start "/path/uvicorn --root-path /context-url --app-dir /root-project main:app --host 0.0.0.0 --port 8000 --proxy-headers --forwarded-allow-ips 192.168.1.45" --name pm2-app-name
